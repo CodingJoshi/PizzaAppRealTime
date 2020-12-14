a883406564712590b869cf8a424981a793b0b04f
Saurabh Joshi <joshisaurabh2610@gmail.com>
2020-12-15 00:47:43 +0530

    cart Login register pages added

--- Server.js
+++ Server.js
@@ -8,14 +8,29 @@ const expressLayout=require('express-ejs-layouts');
 // assests 
 app.use(express.static(__dirname+'/public'));
 
-app.get('/',(req,res)=>{
-    res.render('home');
-})
+
 
 // set ejs.Template engine
 app.use(expressLayout);
 app.set('views',path.join(__dirname,'/resources/views'))
 app.set('view engine','ejs');
+
+
+app.get('/',(req,res)=>{
+    res.render('home');
+})
+
+app.get('/cart',(req,res)=>{
+    res.render('customers/cart');
+})
+
+app.get('/register',(req,res)=>{
+    res.render('auth/register');
+})
+
+app.get('/login',(req,res)=>{
+    res.render('auth/login');
+})
 
 app.listen(PORT,()=>{
     console.log(`server running at http://localhost:${PORT}`);
